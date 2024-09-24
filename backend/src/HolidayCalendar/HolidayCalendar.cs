7557c5f955da0854539a778dccc427a6384b6568
osman-butt <butt.osman@gmail.com>
2024-09-24 21:52:31 +0200

    Refractor backend challenge into backend directory

---
+++ backend/src/HolidayCalendar/HolidayCalendar.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+
+namespace HolidayCalendar;
+public class HolidayCalendar : IHolidayCalendar
+{
+  private IExternalHolidayApi _externalHolidayApi;
+
+  public HolidayCalendar()
+  {
+    _externalHolidayApi = new ExternalHolidayApi();
+  }
+
+  public bool IsHoliday(DateTime date)
+  { 
+    return _externalHolidayApi.IsHoliday(date);
+  }
+
+  public ICollection<DateTime> GetHolidays(DateTime startDate, DateTime endDate)
+  {
+    var holidays = _externalHolidayApi.GetHolidays(startDate, endDate);
+    var holidayDates = new List<DateTime>();
+    foreach (var holiday in holidays)
+    {
+      if (holiday.nationalHoliday) holidayDates.Add(DateTime.Parse(holiday.date));
+    }
+    return holidayDates;
+  }
+}
