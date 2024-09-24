bf685c3cb3c20f2e32f7254c7096822022ad47ea
osman-butt <butt.osman@gmail.com>
2024-09-25 00:48:27 +0200

    Refractor into smaller components

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -18,8 +18,6 @@ const MONTHS = [
   "December",
 ];
 
-const WEEKDAY = ["S", "M", "T", "O", "T", "F", "L"];
-
 export default function Calendar() {
   const [holidays, setHolidays] = useState<HolidayType[]>([]);
 
@@ -34,60 +32,125 @@ export default function Calendar() {
   useEffect(() => {
     // Fetch holidays from API and set state
     getHolidays(today.getFullYear()).then(data => {
-      console.log(data);
       setHolidays(data);
     });
   }, []);
 
   return (
     <div className="bg-[#21222f] text-[#acb3d6] w-full max-h-screen">
-      {/* HEADER */}
       <div className="grid grid-cols-1 gap-10 p-1 md:grid-cols-3 lg:grid-cols-6 md:gap-x-0">
         {dateSixMonths.map((monthOfDates, monthIndex) => (
-          <div key={monthIndex} className="text-center">
-            <div>
+          <div key={monthIndex}>
+            <MonthHeader>
               {displayMonths[monthIndex]} {monthOfDates[0].getFullYear()}
-            </div>
-            {monthOfDates.map((date, dateIndex) => (
-              <div
-                key={dateIndex}
-                className={cn("flex items-center border border-zinc-50/10", {
-                  "bg-[#313447]": date.getDay() === 0,
-                  "bg-[#313446]": holidays.some(holiday => {
-                    // TODO: ONE OFF ERROR - FIXED USING formatDate
-
-                    return (
-                      holiday.date === formatDate(date) &&
-                      holiday.nationalHoliday
-                    );
-                  }),
-                })}
-              >
-                <p
-                  className={cn("w-10 text-sm", {
-                    "bg-[#313447]": date.getDay() === 6, //Check if it is a Saturday
-                  })}
-                >
-                  {WEEKDAY[date.getDay()]}
-                </p>
-                <div className="w-6 text-sm text-right">{date.getDate()}</div>
-                <div className="w-full pl-3 text-xs ">
-                  {
-                    holidays.find(holiday => holiday.date === formatDate(date))
-                      ?.name
-                  }
-                </div>
-                <div className="pl-10 text-right ">
-                  {date.getDay() === 1 && getWeekNumber(date)}
-                </div>
-              </div>
-            ))}
+              {monthOfDates.map((date, dateIndex) => (
+                <MonthContainer key={dateIndex} date={date} holidays={holidays}>
+                  <DayContainer date={date} holidays={holidays} />
+                </MonthContainer>
+              ))}
+            </MonthHeader>
           </div>
         ))}
       </div>
     </div>
   );
 }
+
+const MonthHeader = ({
+  className,
+  ...props
+}: React.HTMLAttributes<HTMLDivElement>) => {
+  return <div className={cn(className, "text-center")} {...props} />;
+};
+
+interface DateWithHolidaysProps extends React.HTMLAttributes<HTMLDivElement> {
+  date: Date;
+  holidays: HolidayType[];
+}
+
+const MonthContainer = ({
+  className,
+  date,
+  holidays,
+  ...props
+}: DateWithHolidaysProps) => {
+  return (
+    <div
+      className={cn(className, {
+        "bg-[#313447]": date.getDay() === 0,
+        "bg-[#313446]": holidays.some(holiday => {
+          return holiday.date === formatDate(date) && holiday.nationalHoliday;
+        }),
+      })}
+      {...props}
+    />
+  );
+};
+
+const DayContainer = ({ className, date, holidays }: DateWithHolidaysProps) => {
+  return (
+    <div
+      className={cn(className, "flex items-center border border-zinc-50/10")}
+    >
+      <DayOfWeek date={date} />
+      <DateDisplay date={date} />
+      <HolidayDisplay date={date} holidays={holidays} />
+      <WeekNumberDisplay date={date} />
+    </div>
+  );
+};
+
+const DayOfWeek = ({ className, date, ...props }: DayProps) => {
+  const WEEKDAY = ["S", "M", "T", "O", "T", "F", "L"] as const;
+
+  return (
+    <p
+      className={cn(className, "w-10 text-sm", {
+        "bg-[#313447]": date.getDay() === 6, // Check if it is a Saturday
+      })}
+      {...props}
+    >
+      {WEEKDAY[date.getDay()]}
+    </p>
+  );
+};
+
+const DateDisplay = ({ className, date, ...props }: DayProps) => {
+  return (
+    <div className={cn(className, "w-6 text-sm text-right")} {...props}>
+      {date.getDate()}
+    </div>
+  );
+};
+
+const HolidayDisplay = ({
+  className,
+  date,
+  holidays,
+  ...props
+}: DateWithHolidaysProps) => {
+  const holidayName = holidays.find(
+    holiday => holiday.date === formatDate(date)
+  )?.name;
+
+  return (
+    <div className={cn(className, "w-full pl-3 text-xs")} {...props}>
+      {holidayName || ""}
+    </div>
+  );
+};
+
+interface DayProps extends React.HTMLAttributes<HTMLDivElement> {
+  date: Date;
+}
+
+const WeekNumberDisplay = ({ className, date, ...props }: DayProps) => {
+  return (
+    <div className={cn(className, "pl-10 text-right")} {...props}>
+      {date.getDay() === 1 && getWeekNumber(date)}
+    </div>
+  );
+};
 
 // get which half of the year we are in
 const getMonthOffset = (date: Date) => {
