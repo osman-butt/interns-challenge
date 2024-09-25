2bc15676f98062c7681193f95f50fee84c57ad9f
osman-butt <butt.osman@gmail.com>
2024-09-25 12:07:52 +0200

    Register HttpClient

--- api/HolidayApi/Program.cs
+++ api/HolidayApi/Program.cs
@@ -1,6 +1,13 @@
+using HolidayApi.Interfaces;
+using HolidayApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IHolidayService, HolidayService>();
+
+// Add HttpClient to the container
+builder.Services.AddHttpClient<IHolidayService, HolidayService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
