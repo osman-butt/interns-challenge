3fc48db1d2ffc1cf4e8b01d95df8087920384d79
osman-butt <butt.osman@gmail.com>
2024-09-25 03:01:08 +0200

    Change endpoint to use start and end date

--- frontend/settings.ts
+++ frontend/settings.ts
@@ -1,7 +1,12 @@
 const TOKEN = import.meta.env.VITE_SALLING_GROUP_API_TOKEN as string;
-
+const URL = import.meta.env.VITE_SALLING_GROUP_API_URL as string;
 if (!TOKEN) {
   console.error("API token not found");
 }
 
-export const API_TOKEN = import.meta.env.VITE_SALLING_GROUP_API_TOKEN;
+if (!URL) {
+  console.error("API URL not found");
+}
+
+export const API_TOKEN = TOKEN;
+export const API_URL = URL;
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,14 +1,22 @@
 import { HolidayType } from "../types";
-import { API_TOKEN } from "../../Settings";
+import { API_TOKEN, API_URL } from "../../Settings";
 
-const BASE_URL = "https://api.sallinggroup.com";
-const HOLIDAY_ENDPOINT = "/v1/holidays";
+const HOLIDAY_ENDPOINT = "/holidays";
 const TOKEN = API_TOKEN;
 
-const getHolidays = async (year: number): Promise<HolidayType[]> => {
+const getHolidays = async (
+  startDate: Date,
+  endDate: Date
+): Promise<HolidayType[]> => {
+  const start = `${startDate.getFullYear()}-${String(
+    startDate.getMonth() + 1
+  ).padStart(2, "0")}-${String(startDate.getDate()).padStart(2, "0")}`;
+  const end = `${endDate.getFullYear()}-${String(
+    endDate.getMonth() + 1
+  ).padStart(2, "0")}-${String(endDate.getDate()).padStart(2, "0")}`;
   // Replace with actual API URL and parameters
   const response = await fetch(
-    `${BASE_URL}${HOLIDAY_ENDPOINT}?startDate=${year}`,
+    `${API_URL}${HOLIDAY_ENDPOINT}?startDate=${start}&endDate=${end}`,
     {
       headers: {
         Authorization: `Bearer ${TOKEN}`,
